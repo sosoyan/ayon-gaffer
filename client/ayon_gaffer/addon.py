f2ffe9b8dc942a64a60ecdee3370b5553f9e4f5a
Vahan Sosoyan <sosoyan@gmail.com>
2025-01-31 15:34:57 +0400

    Update addon.py

--- client/ayon_gaffer/addon.py
+++ client/ayon_gaffer/addon.py
@@ -10,17 +10,17 @@ class GafferAddon(AYONAddon, IHostAddon):
     """
     Gaffer addon class.
     """
-    
+
     name = "gaffer"
     host_name = "gaffer"
     version = __version__
-    
+
     def add_implementation_envs(self, env, _app):
         """
           Add requirements to GAFFER_EXTENSION_PATHS
         """
         gaf_ext_paths = "GAFFER_EXTENSION_PATHS"
-        
+
         if gaf_ext_paths in env:
             env[gaf_ext_paths] += os.pathsep + GAFFER_HOST_DIR
         else:
@@ -29,7 +29,7 @@ class GafferAddon(AYONAddon, IHostAddon):
     def get_launch_hook_paths(self, app):
         if app.host_name == self.host_name:
             return [os.path.join(GAFFER_HOST_DIR, "hooks")]
-        
+
         return []
 
     def get_workfile_extensions(self):

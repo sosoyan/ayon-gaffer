046791de244d8878ee44e4db0079068486ff35ff
Vahan Sosoyan <sosoyan@gmail.com>
2025-03-31 00:19:41 +0400

    Clean up

--- client/ayon_gaffer/api/__init__.py
+++ client/ayon_gaffer/api/__init__.py
@@ -10,16 +10,11 @@ from .lib import (
     retrieve_context
     )
 
-from .node import (
-    install_nodes
-    )
-
 __all__ = [
     "GafferHost",
     "GafferSignal",
     "GafferScript",
     "setup_project",
     "update_context",
-    "retrieve_context",
-    "install_nodes"
+    "retrieve_context"
     ]

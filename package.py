0881d061583f16c24070938d4d122a67508a2437
Vahan Sosoyan <sosoyan@gmail.com>
2025-09-11 12:31:05 +0400

    project_can_override_addon_version

--- package.py
+++ package.py
@@ -1,7 +1,9 @@
 name = "gaffer"
 title = "Gaffer"
 version = "0.2.0"
+app_host_name = "gaffer"
 client_dir = "ayon_gaffer"
+project_can_override_addon_version = True
 
 ayon_required_addons = {"core"  : ">=1.0.3"}
 ayon_compatible_addons = {}
